80c09192575c45f2384e03feeed58aeea12e08de
Petrovskaya-Svetlana <s7606003@gmail.com>
2023-09-10 10:37:51 +0300

    finish 116

--- weather/views.py
+++ weather/views.py
@@ -6,7 +6,7 @@ def index(request):
     appid = '9168126ab4661d1056c642fcc0606e45'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
     city = 'London'
-    res = requests.get(url.format(city))
+    res = requests.get(url.format(city)).json()
     city_info = {
         'city': city,
         'temp': res['main']['temp'],
