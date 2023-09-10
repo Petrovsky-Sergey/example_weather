adcf3639683f93006da3a02a6946cdf1b5839432
Petrovskaya-Svetlana <s7606003@gmail.com>
2023-09-10 10:05:46 +0300

    finish 115

--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+import requests
+
 
 def index(request):
-    return render(request, 'weather/index.html')
+    appid = '9168126ab4661d1056c642fcc0606e45'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+    city = 'London'
+    res = requests.get(url.format(city))
+    city_info = {
+        'city': city,
+        'temp': res['main']['temp'],
+        'icon': res['weather'][0]['icon']
+    }
+    context = {'info': city_info}
+    return render(request, 'weather/index.html', context)
