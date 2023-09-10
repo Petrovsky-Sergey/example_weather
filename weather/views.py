23d94903303b72c61a1951f265a1182203c1ab27
Petrovskaya-Svetlana <s7606003@gmail.com>
2023-09-10 12:37:10 +0300

    finish 119

--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
 
 
 def index(request):
     appid = '9168126ab4661d1056c642fcc0606e45'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
+
+if(request.method == "POST"):
+    form = CityForm(request.POST)
+    form.save()
+
+    form = CityForm
+
     cities = City.objects.all()
 
     all_cities = []
@@ -19,5 +27,5 @@ def index(request):
         }
 
         all_cities.append(city_info)
-    context = {'all_info': all_cities}
+    context = {'all_info': all_cities, 'form': form}
     return render(request, 'weather/index.html', context)
